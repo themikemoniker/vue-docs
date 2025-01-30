e51a4b7632ffc53b61727b29040225c4963e8a87
themikemoniker <themikemoniker@proton.me>
2025-01-30 00:09:32 -0600

    applied google Lato font

--- my-portfolio/nuxt.config.ts
+++ my-portfolio/nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/css/tailwind.css"], // Ensure this points to the right file
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "anonymous" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" }
+      ]
+    }
+  },
   devtools: { enabled: true }
 })
