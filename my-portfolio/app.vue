147cf3cc91f448d03934ca289ff89c24a183db36
themikemoniker <themikemoniker@proton.me>
2025-01-31 00:01:47 -0600

    finished the my work section

--- my-portfolio/app.vue
+++ my-portfolio/app.vue
@@ -4,12 +4,37 @@
     <Header />
     <Hero />
     <AboutMe />
-    <slot />
+    <section id="showcase" class="py-16 bg-slate-200">
+      <h2 class="text-primary text-3xl mb-8 text-center">
+        My Work
+      </h2>
+      <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+        <project-showcase
+          v-for="project in projects"
+          :key="project.name"
+          :project="project"
+          />
+      </div>
+    </section>
   </div>
 </template>
 
 <script setup lang="ts">
-import Header from './components/Header.vue';
-import Hero from './components/Hero.vue';
-import AboutMe from './components/AboutMe.vue';
+const projects = [
+  {
+    name: "Bigquery News Map",
+    description: "Mapbox map populated by news articles stored and queried with Google BigQuery big data solution.",
+    image: "/images/FIU-screenshot.png"
+  },
+  {
+    name: "Bigquery News Map",
+    description: "Mapbox map populated by news articles stored and queried with Google BigQuery big data solution.",
+    image: "/images/FIU-screenshot.png"
+  },
+  {
+    name: "Bigquery News Map",
+    description: "Mapbox map populated by news articles stored and queried with Google BigQuery big data solution.",
+    image: "/images/FIU-screenshot.png"
+  }
+]
 </script>
